801f3287f8bdbaa77de4660a4de60d8837ab629a
Monica Espinosa <monicadan26@gmail.com>
2025-04-20 16:50:52 -0600

    fix: Actualiza configuración de base en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
   },
-  site: 'https://even-more-effective.github.io',
+  site: 'https://even-more-effective.github.io/effective-altruism-upy.github.io', // Actualiza el site para que coincida con la URL de tu GitHub Pages
+  base: '/effective-altruism-upy.github.io/', // Añade la propiedad base con el nombre de tu repositorio
 });
